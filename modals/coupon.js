7b52fe579d1f0e83c44f7e042fcb4094dd6fe27e
Muhammed Althaf CP <muhammedalthafcp449@gmail.com>
2025-01-12 23:15:12 +0530

    update

--- modals/coupon.js
+++ modals/coupon.js
@@ -6,13 +6,7 @@ const couponSchema = new mongoose.Schema({
     expirationDate: { type: Date, required: true }, // Expiration date for the coupon
     minPurchaseAmount: { type: Number, required: true }, // Minimum purchase amount required to use the coupon
     createdAt: { type: Date, default: Date.now } // Automatically set the creation date
-
-
 });
-
-
-
-
 const Coupon = mongoose.model('Coupon', couponSchema);
 
 module.exports = Coupon;
