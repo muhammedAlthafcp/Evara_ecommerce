f93f088b14334d96373e5b6059bcd421b70ea565
Muhammed Althaf CP <muhammedalthafcp449@gmail.com>
2025-09-19 01:40:16 +0530

    db error fixed

--- config/Connection.js
+++ config/Connection.js
@@ -1,21 +1,15 @@
 const mongoose = require('mongoose');
-require('dotenv').config(); // Load environment variables from .env file
+require('dotenv').config();
 
-const databaseUrl = process.env.DATABASE_URL || 'your-default-connection-string-here';
+const databaseUrl = process.env.DATABASE_URL;
 
-// Connect to MongoDB
-mongoose.connect(databaseUrl, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
-    .then(() => {
-        console.log("Connection to MongoDB was successful");
-    })
-    .catch((error) => {
-        console.error("Error in MongoDB connection", error);
-        process.exit(1); 
-    });
+mongoose.connect(databaseUrl)
+  .then(() => {
+    console.log("✅ Connection to MongoDB was successful");
+  })
+  .catch((error) => {
+    console.error("❌ Error in MongoDB connection", error);
+    process.exit(1);
+  });
 
 module.exports = mongoose;
-
-
--- modals/cartschema.js
+++ modals/cartschema.js
@@ -33,3 +33,4 @@ const cartSchema = new mongoose.Schema({
 const Cart = mongoose.model('Cart', cartSchema);
 
 module.exports = Cart;
+
